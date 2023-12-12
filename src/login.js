e33943301989323e70949ff050ebed7005c3fbc9
Jacob S Peterson <99829241+JacobStPeterson@users.noreply.github.com>
2023-12-12 11:37:22 -0500

    Create login.js

---
+++ src/login.js
@@ -0,0 +1,24 @@
+
+function validateLogin(event) {
+    // Check if the form is being submitted
+    if (event.submitter && event.submitter.type === 'submit') {
+      // Add your login validation logic here
+      alert("Login button pressed");
+  
+      // Return false to prevent the form from submitting
+      return false;
+    }
+  
+    // Continue with form submission for other buttons
+    return true;
+  }
+  
+  function back() {
+    // Add any logic you need for the "Back" button
+    window.location.href = 'MainPage.html';
+  }
+  
+  function forgotPassword() {
+    // Add any logic you need for the "Forgot Password" button
+    alert("Forgot Password button pressed");
+  }
