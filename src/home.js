c79936b6b355e609b4977b4bd1fd48514d8000f8
Jacob S Peterson <jacob.st.peterson@gmail.com>
2023-12-13 14:33:58 -0500

    newest Updated stuff

--- src/home.js
+++ src/home.js
@@ -1,3 +1,16 @@
+
+var header_text = "Wardrobe";
+var dynamic_frame;
+
+function load_iframe(html_filename) {
+    dynamic_frame.src = html_filename;
+}
+
+function reloadHeader() {
+    document.getElementById("header_text").innerHTML = header_text;
+    closeNav();
+}
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -10,4 +23,24 @@ function logout() {
 
     alert("None of the backend has been implemented yet.");
     window.location.href = 'MainPage.html';
+}
+
+function loadWardrobe() {
+    header_text = "Wardrobe";
+    reloadHeader();
+}
+
+function loadLaundry() {
+    header_text = "Laundry";
+    reloadHeader();
+}
+
+function loadWishList() {
+    header_text = "WishList";
+    reloadHeader();
+}
+
+function loadSettings() {
+    header_text = "Settings";
+    reloadHeader();
 }
